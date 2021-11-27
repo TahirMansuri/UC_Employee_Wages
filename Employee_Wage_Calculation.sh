bbfef96c2389e9e03a0a4a6386366aba14b5c5d1
TahirMansuri <tprocks786@gmail.com>
2021-11-27 18:22:37 +0530

    MODIFIED Add new Module for Employee Part Time Wage

--- Employee_Wage_Calculation.sh
+++ Employee_Wage_Calculation.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -x
 
 #Variables
-isPresent=1;
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
 randomCheck=$((RANDOM%2));
 
-if (( $isPresent==$randomCheck ))
+if (( $isFullTime==$randomCheck ))
 then
-	empRatePerHr=20;
 	empHrs=8;
-	salary=$(($empHrs+$empRatePerHr));
+elif (( $isPartTime==$randomCheck ))
+then
+	empHrs=4;
 else
-	salary=0;
+	empHrs=0;
 fi
+salary=$(($empHrs*$empRatePerHr));
