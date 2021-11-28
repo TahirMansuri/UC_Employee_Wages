387f6c01ad853b851549239e9cbc5c1a5de24724
TahirMansuri <tprocks786@gmail.com>
2021-11-28 10:40:20 +0530

    MODIFIED for Adding Loop to calculate Wage for Month

--- Employee_Wage_Calculation.sh
+++ Employee_Wage_Calculation.sh
@@ -3,18 +3,26 @@
 #Variables
 isPartTime=1;
 isFullTime=2;
+totalSalary=0;
 empRatePerHr=20;
-randomCheck=$((RANDOM%3));
+numWorkingDays=20;
 
-case $randomCheck in
-	$isFullTime)
-		empHrs=8;
-	;;
-	$isPartTime)
-		empHrs=4;
-	;;
-	*)
-		empHrs=0;
-	;;
-esac
-salary=$(($empHrs*$empRatePerHr));
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+
+	randomCheck=$((RANDOM%3));
+	case $randomCheck in
+		$isFullTime)
+			empHrs=8;
+		;;
+		$isPartTime)
+			empHrs=4;
+		;;
+		*)
+			empHrs=0;
+		;;
+	esac
+
+	salary=$(($empHrs*$empRatePerHr));
+	totalSalary=$(($totalSalary+$salary));
+done
